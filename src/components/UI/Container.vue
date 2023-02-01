b59d2490a34c029595a7e1f6388de39c2b5775c7
risbo <b.rozumnyi@ringostat.com>
2023-02-01 16:03:19 +0100

    created a container component

---
+++ src/components/UI/Container.vue
@@ -0,0 +1,43 @@
+<template>
+	<div class="container">
+		<slot></slot>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Container'
+}
+</script>
+
+<style>
+.container {
+	padding: 0 16px;
+	max-width: 300px;
+	margin: 0 auto;
+}
+
+@media screen and (min-width: 380px) {
+	.container {
+		max-width: 340px;
+	}
+}
+
+@media screen and (min-width: 580px) {
+	.container {
+		max-width: 550px;
+	}
+}
+
+@media screen and (min-width: 920px) {
+	.container {
+		max-width: 820px;
+	}
+}
+
+@media screen and (min-width: 1200px) {
+	.container {
+		max-width: 1120px;
+	}
+}
+</style>
--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -2,5 +2,6 @@ import Button from "./Button.vue";
 import Input from "./Input.vue";
 import Checkbox from "./Checkbox.vue";
 import ShowError from "./ShowError.vue";
+import Container from "./Container.vue";
 
-export default [Button, Input, Checkbox, ShowError];
+export default [Button, Input, Checkbox, ShowError, Container];
