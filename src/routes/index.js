4e80c785dd38010c181ad60516ec26485a13d712
risbo <b.rozumnyi@ringostat.com>
2023-02-07 18:13:34 +0100

    created the NotFound page

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,12 +4,14 @@ const HomePage = () => import("../views/HomePage.vue");
 const TodosCoockbook = () => import("../views/TodosCoockbook.vue");
 const TimeCalculator = () => import("../views/TimeCalculator.vue");
 const TestLayout = () => import("../views/TestLayout.vue");
+const NotFound = () => import("../views/NotFound.vue");
 
 export const routes = [
 	{ path: "/", component: HomePage },
   { path: "/todos-coockbook", component: TodosCoockbook },
 	{ path: "/time-calculator", component: TimeCalculator },
   { path: "/layout", component: TestLayout },
+  { path: "/:pathMathc(.*)*", name: "NotFound", component: NotFound },
 ];
 
 export const router = createRouter({
---
+++ src/views/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+	<PageLayout><template v-slot:content>
+			<Container>
+				<h2 class="mt40">Page Not Found</h2>
+				<button @click="$router.back()">Go back</button>
+			</Container>
+		</template></PageLayout>
+</template>
