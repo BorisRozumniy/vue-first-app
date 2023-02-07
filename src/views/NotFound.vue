25d4ea1240e563857b1450ae4d34855ba6e55de8
risbo <b.rozumnyi@ringostat.com>
2023-02-07 18:31:23 +0100

    styled the NotFound page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -2,7 +2,18 @@
 	<PageLayout><template v-slot:content>
 			<Container>
 				<h2 class="mt40">Page Not Found</h2>
-				<button @click="$router.back()">Go back</button>
+				<Button @click="$router.back()">Go back</Button>
 			</Container>
 		</template></PageLayout>
 </template>
+
+<style scoped>
+h2 {
+	text-align: center;
+    margin-bottom: 40px;
+}
+button {
+	display: block;
+	margin: 0 auto;
+}
+</style>
