137e6c137aba03d238b4c4b76d1ff7e18f58e1f0
risbo <b.rozumnyi@ringostat.com>
2023-02-15 16:26:30 +0100

    created todo input form

--- src/components/SecondTodoList.vue
+++ src/components/SecondTodoList.vue
@@ -1,4 +1,28 @@
 <template>
-	<input type="text">
-	<button>add</button>
+	<input v-model="todo.title" type="text" placeholder="title">
+	<button @click="createTodo">add</button>
 </template>
+
+<script>
+export default {
+	data() {
+		return {
+			todo: {
+				title: '',
+				id: null,
+				isDone: false
+			}
+		}
+	},
+	methods: {
+		createTodo() {
+			this.todo.id = Date.now();
+			this.$emit('create', this.todo)
+			console.log(this.todo);
+			this.todo = {
+				title: '', isDone: false,
+			}
+		}
+	}
+}
+</script>
