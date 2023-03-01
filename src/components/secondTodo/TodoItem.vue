f3d3eb8ec582e731ab0e01cb76a7c50a97eb788c
risbo <b.rozumnyi@ringostat.com>
2023-03-01 05:26:06 +0100

    styled the TodoItem component

--- src/components/secondTodo/TodoItem.vue
+++ src/components/secondTodo/TodoItem.vue
@@ -1,7 +1,13 @@
 <template>
 	<div class="todo-item">
-		<b>{{ todo.title }}</b>
-		<span v-if="todo.isDone">+</span>
+		<div class="content">
+			<b>{{ todo.title }}</b>
+			<span v-if="todo.isDone">+</span>
+		</div>
+		<div class="buttons">
+			<button class="edit">edit</button>
+			<button class="remove">remove</button>
+		</div>
 	</div>
 </template>
 
@@ -16,8 +22,38 @@ export default {
 <style scoped>
 .todo-item {
 	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
 	border: solid 2px;
-	padding: 15px;
 	margin-top: 4px;
+}
+
+.content {
+	padding: 15px;
+}
+.buttons {
+	padding: 15px;
+}
+
+.buttons button {
+	min-width: 135px;
+	color: white;
+}
+
+.buttons :nth-child(1) {
+	margin-right: 8px;
+}
+
+.remove {
+	background-color: red;
+}
+.edit {
+	background-color: green;
+}
+
+@media screen and (min-width: 580px) {
+.todo-item{
+	flex-direction: row;
+}
 }
 </style>
