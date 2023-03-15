2ad594d17c7b49d5cb4767ad5a67ec263f7f2523
risbo <b.rozumnyi@ringostat.com>
2023-03-15 20:55:33 +0100

    styled buttons; added icons for buttons

--- src/components/secondTodo/TodoItem.vue
+++ src/components/secondTodo/TodoItem.vue
@@ -1,27 +1,35 @@
 <template>
 	<div class="todo-item" :id="'todo-item' + todo.id">
-		<div class="edit" v-if="editingTodo">
+		<div class="edit-block" v-if="editingTodo">
 			<input type="text" v-model="editingTodo.title">
 		</div>
-		<div class="content" v-else>
+		<div class="content-block" v-else>
 			<b>{{ todo.title }}</b>
 			<span v-if="todo.isDone">+</span>
 		</div>
 		<div class="buttons">
 			<template v-if="editingTodo">
-				<button class="save" @click="onEditSave">save</button>
+				<button class="save" @click="onEditSave"><img :src="saveIcon" /></button>
 			</template>
 			<template v-else>
-				<button class="edit" @click="onEdit(todo)">edit</button>
-				<button class="remove" @click="onRemove(todo.id)">remove</button>
+				<button class="edit" @click="onEdit(todo)"><img :src="editIcon" alt="edit"></button>
+				<button class="remove" @click="onRemove(todo.id)"><img :src="reremoveIcon" alt="remove"></button>
 			</template>
 		</div>
 	</div>
 </template>
 
 <script>
+import saveIcon from "../../assets/save-svgrepo-com.svg"
+import editIcon from "../../assets/edit-2-svgrepo-com.svg"
+import reremoveIcon from "../../assets/remove-all-svgrepo-com.svg"
 
 export default {
+	setup() {
+		return {
+			editIcon, reremoveIcon, saveIcon
+		}
+	},
 	props: {
 		todo: { type: Object, requered: true }
 	},
@@ -55,30 +63,48 @@ export default {
 	margin-top: 4px;
 }
 
-.content {
+.content-block {
 	padding: 15px;
+}
+.content-block, .edit-block {
+	padding: 15px;
+	width: 80%;
+}
+.edit-block input {
+	font-family: 'Courier New', Courier, monospace;
+	font-size: 22px;
+	font-weight: bold;
+	color: #213547;
 }
 .buttons {
 	padding: 15px;
+	display: flex;
+	width: 14%;
+	min-width: 152px;
+	justify-content: flex-end;
 }
 
 .buttons button {
-	min-width: 135px;
-	color: white;
+	border-radius: 50%;
+	margin-left: 8px;
+	width: 55px;
+	height: 55px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
 }
 
-.buttons :nth-child(1) {
-	margin-right: 8px;
+button img {
+	width: 25px;
 }
-
 .remove {
-	background-color: red;
+	background-color: #ff6060;
 }
 .edit {
-	background-color: green;
+	background-color: #28c728;
 }
 .save {
-	background-color: rgb(35, 75, 255);
+	background-color: #6379dd;
 }
 
 @media screen and (min-width: 580px) {
